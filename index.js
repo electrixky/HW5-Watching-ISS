4b86bc47718aeafae6d0027a1232943e6d86bbf5
Tetiana Tkachuk <tanyatkachuk2401@gmail.com>
2021-07-06 13:49:56 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// async function myFunc() {
+// 	try {
+// 		let response = await fetch("https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits", {
+// 		method: "GET",
+// 		headers: {
+// 			"Content-type": "aplication/json"
+// 		}
+// 	})
+// 	const parsed = await response.json()
+// 	console.log(parsed[0].author)
+// 	} catch(ex) {
+// 		console.log(ex)
+// 	}
+// }
+
+// myFunc()
